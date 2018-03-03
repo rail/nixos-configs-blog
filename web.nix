983a1a77a879ab9d17fa21ab6016e6c224f0fd0e
Rail Aliiev <rail@mozilla.com>
2018-03-03 14:45:08 +0000

    nginx optimization

--- web.nix
+++ web.nix
@@ -1,35 +1,40 @@
 { config,  ... }:
 
 {
-  services.nginx.enable = true;
-  services.nginx.virtualHosts = {
-    "rail.merail.ca" = {
-      forceSSL = true;
-      enableACME = true;
-      root = "/home/www/rail.merail.ca/";
-    };
-    "img.lgtm.ca" = {
-      forceSSL = true;
-      enableACME = true;
-      root = "/home/www/img.lgtm.ca";
-      serverAliases = [ "img.merail.ca" ];
-      locations = {
-        "/" = {
-          extraConfig = ''
-            autoindex on; 
-          '';
-	};
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+    virtualHosts = {
+      "rail.merail.ca" = {
+        forceSSL = true;
+        enableACME = true;
+        root = "/home/www/rail.merail.ca/";
       };
-    };
-    "merail.ca" = {
-      forceSSL = true;
-      enableACME = true;
-      globalRedirect = "rail.merail.ca";
-    };
-    "lgtm.ca" = {
-      forceSSL = true;
-      enableACME = true;
-      globalRedirect = "rail.merail.ca";
+      "img.lgtm.ca" = {
+        forceSSL = true;
+        enableACME = true;
+        root = "/home/www/img.lgtm.ca";
+        serverAliases = [ "img.merail.ca" ];
+        locations = {
+          "/" = {
+            extraConfig = ''
+              autoindex on; 
+            '';
+	  };
+        };
+      };
+      "merail.ca" = {
+        forceSSL = true;
+        enableACME = true;
+        globalRedirect = "rail.merail.ca";
+      };
+      "lgtm.ca" = {
+        forceSSL = true;
+        enableACME = true;
+        globalRedirect = "rail.merail.ca";
+      };
     };
   };
 }
